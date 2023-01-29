cad30d62c4eeda9c57b1896a022a800c0683b5e6
Sudarshan Vijay <vijays@fysik.dtu.dk>
2023-01-29 14:39:19 -0800

    First implementation of individual calculation

--- plot_max_error.py
+++ plot_max_error.py
@@ -1,5 +1,7 @@
 import os
 
+import shutil
+
 import json
 
 from string import Template
@@ -23,6 +25,10 @@ import matplotlib.pyplot as plt
 
 from template_reactions import templates
 
+from plot_params import get_plot_params
+
+get_plot_params()
+
 
 def get_cli_args():
     """Get command line arguments."""
@@ -31,6 +37,164 @@ def get_cli_args():
         "--resolution", type=int, default=10, help="Number of points to plot."
     )
     return parser.parse_args()
+
+
+def evaulate_data(
+    data: pd.DataFrame,
+    accuracy: float,
+    ax: plt.Axes,
+    success_settings: dict = {},
+    failure_settings: dict = {},
+) -> Tuple[float]:
+    """Plot the data to compare two solvers."""
+
+    # Remove rows where the error is equal to 0
+    data = data[data["error"] != 0]
+
+    # Store success and failure data
+    success = 0
+    failure = 0
+
+    for (desc1, desc2), dat in data.groupby(["descriptor1", "descriptor2"]):
+
+        # Split the pandas dataframe into a series of dataframes where the
+        # "iteration" column is not strictly monotonically increasing
+        chunks = np.split(dat, np.where(np.diff(dat["iteration"]) < 0)[0] + 1)
+
+        # Iterate over the chunks
+        for chunk in chunks:
+            # Make sure that the chunks are ordered with respect to iteration
+            chunk = chunk.sort_values(by=["iteration"])
+            # Check if the error of the last row is lower than the accuracy
+            if chunk["error"].iloc[-1] < accuracy:
+                success += 1
+                ax.plot(chunk["iteration"], chunk["error"], **success_settings)
+            else:
+                # Make sure that none of the errors are lower than the accuracy
+                assert not any(chunk["error"] < accuracy)
+                failure += 1
+                ax.plot(chunk["iteration"], chunk["error"], **failure_settings)
+
+    return success, failure
+
+
+def parse_run(datadir: str, solver: str, reaction: str, ax, i, convergence_data):
+    """Parse the calculation"""
+
+    if not os.path.isfile(os.path.join(datadir, "error_log.csv")):
+        logger.warning(f"File {os.path.join(datadir, 'error_log.csv')} does not exist.")
+
+        return
+
+    # Use pandas to read the csv file
+    data = pd.read_csv(os.path.join(datadir, "error_log.csv"))
+    # Set labels for data
+    data.columns = ["descriptor1", "descriptor2", "iteration", "error"]
+
+    # Get the accuracy of the solver
+    with open(os.path.join(datadir, "solver_specifics.json")) as f:
+        solver_specifics = json.load(f)
+    ACCURACY = solver_specifics["tolerance"]
+    logger.info("Accuracy: %s", ACCURACY)
+
+    # Get the data to plot
+    success, failure = evaulate_data(
+        data,
+        accuracy=ACCURACY,
+        ax=ax[i],
+    )
+
+    # Store the data in the dataframe
+    data_to_df = {
+        "solver": solver,
+        "reaction": reaction,
+        "success": success,
+        "failure": failure,
+    }
+
+    # Concatenate the data to the dataframe
+    convergence_data = pd.concat(
+        [convergence_data, pd.DataFrame(data_to_df, index=[0])]
+    )
+
+    return convergence_data
+
+
+def run_and_parse_calculation(
+    templates,
+    reaction_name: str,
+    desc1: float,
+    desc2: float,
+    basedir: str,
+    solver_specifics: Dict[str, Any],
+    solvers: List[str],
+    ax: plt.Axes,
+    convergence_data: pd.DataFrame,
+):
+    """Run the single point calculation if the folder is not already present."""
+
+    # Read the common template files for the reactions
+    template = Template(templates["mkm_" + reaction_name])
+    mkm_job_template = Template(templates["mkm_job"])
+
+    # Create the input file for the reaction
+    input_file = template.substitute(
+        desc1=desc1,
+        desc2=desc2,
+    )
+
+    mkm_job = mkm_job_template.substitute()
+
+    # Create the directory for the reaction
+    for idx, solver in enumerate(solvers):
+
+        datadir = os.path.join(
+            basedir, reaction_name, solver, f"desc1_{desc1}_desc2_{desc2}"
+        )
+
+        if not os.path.exists(datadir):
+            os.makedirs(datadir)
+        else:
+            logger.info(f"Skipping {reaction_name} {desc1} {desc2} {solver}")
+            convergence_data = parse_run(
+                datadir, solver, reaction_name, ax, idx, convergence_data
+            )
+            continue
+
+        # Write out a json file with the desc1 and desc2 values
+        with open(os.path.join(datadir, "desc1_desc2.json"), "w") as f:
+            json.dump({"desc1": desc1, "desc2": desc2}, f)
+
+        # Write out the input file from the template
+        with open(os.path.join(datadir, "input.mkm"), "w") as f:
+            f.write(input_file)
+
+        # Write out the solver specifics file
+        with open(os.path.join(datadir, "solver_specifics.json"), "w") as f:
+            json.dump(solver_specifics, f)
+
+        # Write out the mkm_job.py file from the template
+        with open(os.path.join(datadir, "mkm_job.py"), "w") as f:
+            f.write(mkm_job)
+
+        # Copy the energies file for the reaction to the required directory
+        # Make all letters in reaction_name lowercase
+        shutil.copy(
+            os.path.join("input_files", f"energies_{reaction_name.lower()}.txt"),
+            os.path.join(datadir, "energies.txt"),
+        )
+
+        # Execute the mkm_job.py file
+        current_dir = os.getcwd()
+        os.chdir(datadir)
+        os.system("python mkm_job.py")
+        os.chdir(current_dir)
+
+        convergence_data = parse_run(
+            datadir, solver, reaction_name, ax, idx, convergence_data
+        )
+
+    return convergence_data
 
 
 if __name__ == "__main__":
@@ -60,40 +224,24 @@ if __name__ == "__main__":
 
         mesh_desc1, mesh_desc2 = np.meshgrid(desc1_range, desc2_range)
 
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True)
+
+        # Create a dataframe to store the data
+        convergence_data = pd.DataFrame(
+            columns=["solver", "reaction", "success", "failure"]
+        )
+
         for desc1, desc2 in zip(mesh_desc1.flatten(), mesh_desc2.flatten()):
 
-            # Read the common template files for the reactions
-            template = Template(templates["mkm_" + reaction_name])
-            mkm_job_template = Template(templates["mkm_job"])
-
-            # Create the input file for the reaction
-            input_file = template.substitute(
-                desc1=desc1,
-                desc2=desc2,
+            convergence_data = run_and_parse_calculation(
+                templates,
+                reaction_name,
+                desc1,
+                desc2,
+                basedir,
+                solver_specifics,
+                solvers,
+                ax,
+                convergence_data,
             )
-
-            mkm_job = mkm_job_template.substitute()
-
-            # Create the directory for the reaction
-            for solver in solvers:
-                datadir = os.path.join(
-                    basedir, reaction_name, solver, f"desc1_{desc1}_desc2_{desc2}"
-                )
-                if not os.path.exists(datadir):
-                    os.makedirs(datadir)
-
-                # Write out a json file with the desc1 and desc2 values
-                with open(os.path.join(datadir, "desc1_desc2.json"), "w") as f:
-                    json.dump({"desc1": desc1, "desc2": desc2}, f)
-
-                # Write out the input file from the template
-                with open(os.path.join(datadir, "input.mkm"), "w") as f:
-                    f.write(input_file)
-
-                # Write out the solver specifics file
-                with open(os.path.join(datadir, "solver_specifics.json"), "w") as f:
-                    json.dump(solver_specifics, f)
-
-                # Write out the mkm_job.py file from the template
-                with open(os.path.join(datadir, "mkm_job.py"), "w") as f:
-                    f.write(mkm_job)
+        print(convergence_data)
--- template_reactions.py
+++ template_reactions.py
@@ -4,47 +4,47 @@ templates[
     "mkm_job"
 ] = r"""
 
-    from catmap import ReactionModel
-    import os
-    import json
+from catmap import ReactionModel
+import os
+import json
 
-    mkm_file = 'input.mkm'
-    model = ReactionModel(setup_file=mkm_file)
+mkm_file = 'input.mkm'
+model = ReactionModel(setup_file=mkm_file)
 
-    # Decide on the solver based on the current
-    # working directory.
-    folder_name = os.getcwd().split('/')[-1]
-    if 'coverage' in folder_name:
-        # This is default solver.
-        model.use_numbers_solver = False
-        model.fix_x_star = True 
-    elif 'numbers_fix_xstar' in folder_name:
-        # This is the numbers solver with fixed x*=1
-        model.use_numbers_solver = True
-        model.fix_x_star = True
-    elif 'numbers_free_xstar' in folder_name:
-        # This is the numbers solver with free x*
-        model.use_numbers_solver = True
-        model.fix_x_star = False
-    else:
-        raise ValueError('Unknown folder name. '
-                        'Use coverage or numbers_fix_xstar or numbers_free_xstar.')
+# Decide on the solver based on the current
+# working directory.
+folder_name = os.getcwd().split('/')[-2]
+if 'coverage' in folder_name:
+    # This is default solver.
+    model.use_numbers_solver = False
+    model.fix_x_star = True 
+elif 'numbers_fix_xstar' in folder_name:
+    # This is the numbers solver with fixed x*=1
+    model.use_numbers_solver = True
+    model.fix_x_star = True
+elif 'numbers_free_xstar' in folder_name:
+    # This is the numbers solver with free x*
+    model.use_numbers_solver = True
+    model.fix_x_star = False
+else:
+    raise ValueError('Unknown folder name. '
+                    'Use coverage or numbers_fix_xstar or numbers_free_xstar.')
 
-    # Import the json file with the solver specifics
-    with open('solver_specifics.json') as f:
-        solver_specifics = json.load(f)
+# Import the json file with the solver specifics
+with open('solver_specifics.json') as f:
+    solver_specifics = json.load(f)
 
-    # Use the same solver specifications for all test cases.
-    model.decimal_precision = solver_specifics['decimal_precision'] 
-    model.tolerance = solver_specifics['tolerance']
-    model.max_rootfinding_iterations = solver_specifics['max_rootfinding_iterations']
-    model.max_bisections = solver_specifics['max_bisections']
-    model.max_damping_iterations = solver_specifics['max_damping_iterations']
+# Use the same solver specifications for all test cases.
+model.decimal_precision = solver_specifics['decimal_precision'] 
+model.tolerance = solver_specifics['tolerance']
+model.max_rootfinding_iterations = solver_specifics['max_rootfinding_iterations']
+model.max_bisections = solver_specifics['max_bisections']
+model.max_damping_iterations = solver_specifics['max_damping_iterations']
 
-    # Run in DEBUG mode
-    model.DEBUG = True
+# Run in DEBUG mode
+model.DEBUG = True
 
-    model.run()
+model.run()
 
 """
 
@@ -52,39 +52,41 @@ templates[
     "mkm_CO_oxidation"
 ] = r"""
 
-    rxn_expressions = [
-                '*_s + CO_g -> CO*', 
-                '2*_s + O2_g <-> O-O* + *_s -> 2O*',
-                'CO* +  O* <-> O-CO* + * -> CO2_g + 2*',
-                    ]
+rxn_expressions = [
+            '*_s + CO_g -> CO*', 
+            '2*_s + O2_g <-> O-O* + *_s -> 2O*',
+            'CO* +  O* <-> O-CO* + * -> CO2_g + 2*',
+                ]
 
-    surface_names = ['Pt', 'Ag', 'Cu','Rh','Pd','Au','Ru','Ni'] 
+surface_names = ['Pt', 'Ag', 'Cu','Rh','Pd','Au','Ru','Ni'] 
 
-    descriptor_names= ['O_s','CO_s'] #descriptor names
+descriptor_names= ['O_s','CO_s'] #descriptor names
 
-    descriptor_ranges = [[${desc1}, ${desc1}],[${desc2}, ${desc2}]]
+descriptor_ranges = [[${desc1}, ${desc1}],[${desc2}, ${desc2}]]
 
-    temperature = 500 #Temperature of the reaction
+temperature = 500 #Temperature of the reaction
 
-    species_definitions = {}
-    species_definitions['CO_g'] = {'pressure':1.} #define the gas pressures
-    species_definitions['O2_g'] = {'pressure':1./3.}
-    species_definitions['CO2_g'] = {'pressure':0}
-    species_definitions['s'] = {'site_names': ['111'], 'total':1} #define the sites
+resolution = 1
 
-    data_file = 'data.pkl'
+species_definitions = {}
+species_definitions['CO_g'] = {'pressure':1.} #define the gas pressures
+species_definitions['O2_g'] = {'pressure':1./3.}
+species_definitions['CO2_g'] = {'pressure':0}
+species_definitions['s'] = {'site_names': ['111'], 'total':1} #define the sites
 
-    # Parser parameters
+data_file = 'data.pkl'
 
-    input_file = 'energies.txt'
+# Parser parameters
 
-    # Scaler parameters
-    gas_thermo_mode = "shomate_gas"
-    adsorbate_thermo_mode = "frozen_adsorbate"
-    scaling_constraint_dict = {
-                            'O_s':['+',0,None],
-                            'CO_s':[0,'+',None],
-                            'O-CO_s':'initial_state',
-                            'O-O_s':'final_state',
-                            }
+input_file = 'energies.txt'
+
+# Scaler parameters
+gas_thermo_mode = "shomate_gas"
+adsorbate_thermo_mode = "frozen_adsorbate"
+scaling_constraint_dict = {
+                        'O_s':['+',0,None],
+                        'CO_s':[0,'+',None],
+                        'O-CO_s':'initial_state',
+                        'O-O_s':'final_state',
+                        }
     """
